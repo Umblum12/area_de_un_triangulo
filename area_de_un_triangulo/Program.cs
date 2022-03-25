445f3f3387b273686608b66c04f07ef0a1291367
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:21:39 -0700

    Agregar archivos de proyecto.

---
+++ area_de_un_triangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace area_de_un_triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarado de variables
+            int b = 25;
+            int a = 60;
+            int area;
+
+            //Colocar el rocedimiento
+            area = (b * a) / 2;
+
+            //Imprimir en pantalla el resultado
+            Console.WriteLine("El área del triángulo es de: " + area);
+            Console.ReadKey(true);
+
+        }
+    }
+}
